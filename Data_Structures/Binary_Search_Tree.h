724fd6a362bcf17dc226b6e0a7fc252d922e249c
Zain Wajid <u2023775@giki.edu.pk>
2024-12-21 21:01:15 +0500

    21/12/2024

--- Data_Structures/Binary_Search_Tree.h
+++ Data_Structures/Binary_Search_Tree.h
@@ -4,8 +4,8 @@
 #include <iostream>
 using namespace std;
 
-//Value at left of B_Node is smaller
-//Value at right of B_Node is bigger
+// Value at left of B_Node is smaller
+// Value at right of B_Node is bigger
 
 struct B_Node
 {
@@ -16,8 +16,8 @@ struct B_Node
     B_Node(int val) : data(val) , left(nullptr) , right(nullptr) {}
 };
 
-//Recursively calls itself with a new B_Node until it reaches an empty B_Node where value can be inserted
-//Return a B_Node pointer which is inserted at the left or right pointers
+// Recursively calls itself with a new B_Node until it reaches an empty B_Node where value can be inserted
+// Return a B_Node pointer which is inserted at the left or right pointers
 B_Node* insert(B_Node* root , int val)
 {
     //Insert new B_Node if position found
@@ -26,7 +26,7 @@ B_Node* insert(B_Node* root , int val)
         return new B_Node(val);
     }
 
-    //Recursively call itself with a new B_Node until nullptr is found
+    // Recursively call itself with a new B_Node until nullptr is found
     if(val < root->data)
     {
         root->left = insert(root->left , val);
@@ -46,13 +46,13 @@ bool search(B_Node* root , int val)
         return false;
     }
 
-    //Check value of current B_Node
+    // Check value of current B_Node
     if(val == root->data)
     {
         return true;
     }
 
-    //Continue searching left or right depending on value being greater or larger than B_Node data
+    // Continue searching left or right depending on value being greater or larger than B_Node data
     if(val < root->data)
     {
         return search(root->left , val);
@@ -65,8 +65,8 @@ bool search(B_Node* root , int val)
 
 //TRAVERSAL
 
-//1. Inorder : Left -> Root -> Right, Sorted Order
-//Traverses through left tree first
+// 1. Inorder : Left -> Root -> Right, Sorted Order
+// Traverses through left tree first
 void in_order(B_Node* root)
 {
     if(root == nullptr)
@@ -78,7 +78,7 @@ void in_order(B_Node* root)
     in_order(root->right);
 }
 
-//2. Preorder : Root -> Left -> Right
+// 2. Preorder : Root -> Left -> Right
 void pre_order(B_Node* root)
 {
     if(root == nullptr) 
@@ -90,7 +90,7 @@ void pre_order(B_Node* root)
     pre_order(root->right);
 }
 
-//3. Postorder : Left -> Right -> Root
+// 3. Postorder : Left -> Right -> Root
 void post_order(B_Node* root)
 {
     if(root == nullptr)
@@ -102,7 +102,7 @@ void post_order(B_Node* root)
     cout << root->data << " ";
 }
 
-//4. Level order : Breadth First Search(BFS)
+// 4. Level order : Breadth First Search(BFS)
 void level_order(B_Node* root)
 {
     if(root == nullptr)
@@ -136,7 +136,8 @@ void level_order(B_Node* root)
 }
 
 B_Node* min (B_Node* root)
-{
+{   
+    // Keep checking until the leftmost B_Node is found
     while(root && root->left != nullptr)
     {
         root = root->left;
@@ -151,7 +152,7 @@ B_Node* delete_B_Node(B_Node* root , int key)
         return root;
     }
 
-    //Traverse down the tree until value matches key
+    // Traverse down the tree until value matches key
     if(key < root->data)
     {
         root->left = delete_B_Node(root->left, key);
@@ -161,11 +162,11 @@ B_Node* delete_B_Node(B_Node* root , int key)
         root->right = delete_B_Node(root->right , key);
     }
 
-    //Else runs when key matches value, B_Node is one to be deleted
+    // Else runs when key matches value, B_Node is one to be deleted
     else
     {
-        //Case B_Node has one or no children
-        //Replace the B_Node with either its inorder successor or predecessor
+        // Case B_Node has one or no children
+        // Replace the B_Node with either its inorder successor or predecessor
         if(root->left == nullptr)
         {
             B_Node* temp = root->right;
@@ -179,14 +180,14 @@ B_Node* delete_B_Node(B_Node* root , int key)
             return temp;
         }
 
-        //Case B_Node has 2 children
-        //Find the smallest value on the right subtree
+        // Case B_Node has 2 children
+        // Find the smallest value on the right subtree
         B_Node* temp = min(root->right);
 
-        //Change the current B_Nodes data to the smallest value in the right subtree
+        // Change the current B_Nodes data to the smallest value in the right subtree
         root->data = temp->data;
 
-        //Delete inorder successor
+        // Delete inorder successor
         root->right = delete_B_Node(root->right , temp->data);
     }
 
