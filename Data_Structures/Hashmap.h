e9f8c9d4d07a691f500f5f57b861411cc659fd2e
Zain Wajid <u2023775@giki.edu.pk>
2024-12-21 21:14:38 +0500

    21/12/2024

--- Data_Structures/Hashmap.h
+++ Data_Structures/Hashmap.h
@@ -1,21 +1,19 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-#include <string>
-#include <cstdlib>
-#include <ctime>
 #include <list>
-#include <iostream>
-
 
 using namespace std;
 
 // Template for the custom Hashmap
 template <typename K, typename V>
+
 class Hashmap
 {
     private:
+        // Size of hashmap
         int table_size;
+        // List of pairs
         list<pair<K , V>>* table;
 
         int hash_function(K key) const
@@ -25,35 +23,48 @@ class Hashmap
 
     public:
 
+        // Constructor
         Hashmap(int size) : table_size(size)
         {
             table = new list<pair<K, V>>[table_size];
         }
 
+        // Destructor
         ~Hashmap()
         {
             delete[] table;
         }
 
-        void insert(K key, const V& value)
+        // Insert a key value pair
+        // By design there can be no duplicate keys due to the vehicle ids being unique
+        void insert(K key , const V& value)
         {
+            // Get the index of the key
             int index = hash_function(key);
+
+            // Check if key already exists
             for (auto& pair : table[index])
-            {
+            {   
+                // If the key exists , update the value by adding another value for that key
                 if (pair.first == key)
                 {
                     pair.second = value; 
                     return;
                 }
             }
+
+            // If key does not exist, add a new pair
             table[index].emplace_back(key, value);
         }
 
         bool find(K key) const
-        {
+        {   
+            // Get the index of the key
             int index = hash_function(key);
+
+            // Check if key exists
             for (const auto& pair : table[index])
-            {
+            {   
                 if (pair.first == key)
                 {
                     return true;
